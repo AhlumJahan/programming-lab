5a554e6ce5d14ba1c013dca523e3c360d34b52fa
AhlumJahan <ahlumjahan@gmail.com>
2025-06-02 19:34:31 +0530

    lab 5

---
+++ arr1.c
@@ -0,0 +1,15 @@
+// display contents of an integer array
+#include <stdio.h>
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("array elements are:\n");
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
