18c51d7a14a85b2fc0d36368e4b4f22e06c24632
AhlumJahan <ahlumjahan@gmail.com>
2025-06-02 19:36:53 +0530

    lab 5

---
+++ arr5.c
@@ -0,0 +1,12 @@
+// input a string from the user and display the same
+#include <stdio.h>
+int main()
+{
+
+    char str[100];
+    printf("enter your string");
+    scanf("%s", &str);
+    printf("your string is %s", str);
+
+    return 0;
+}
