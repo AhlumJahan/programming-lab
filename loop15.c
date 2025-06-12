ca7d5bc2e80f390271337de4164b2dac5ac4cf6d
AhlumJahan <ahlumjahan@gmail.com>
2025-06-12 20:00:16 +0530

    h/w

---
+++ loop15.c
@@ -0,0 +1,16 @@
+// natural no.in reverse while loop
+#include <stdio.h>
+int main()
+{
+
+    int n;
+    printf("enter n:");
+    scanf("%d", &n);
+    while (n >= 1)
+    {
+        printf("%d\n", n);
+        n--;
+    }
+
+    return 0;
+}
