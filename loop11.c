9faffc456b3c4d655c3f4502b2b4ba6592bc8c73
AhlumJahan <ahlumjahan@gmail.com>
2025-06-12 19:57:34 +0530

    h/w

---
+++ loop11.c
@@ -0,0 +1,15 @@
+// 2 to 10 table
+#include <stdio.h>
+int main()
+{
+
+    int n, i;
+    printf("enter n:");
+    scanf("%d", &n);
+    for (i = 1; i <= 10; i++)
+    {
+        printf(" %dx%d=%d \n", n, i, n * i);
+    }
+
+    return 0;
+}
