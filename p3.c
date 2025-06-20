58d6c7a48d6423db7411e5819b16c793091ca16a
AhlumJahan <ahlumjahan@gmail.com>
2025-06-20 12:25:40 +0530

    lab 7

---
+++ p3.c
@@ -0,0 +1,16 @@
+// display a string in reverse using pointers
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[] = "pointer";
+    char *ptr = str;
+    int len = strlen(str);
+    printf("string in reverse :");
+    for (int i = len - 1; i >= 0; i--)
+    {
+        printf("%c", *(ptr + i));
+    }
+
+    return 0;
+}
