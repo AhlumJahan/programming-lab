e70765da0cab7e7a22b2a25663079923a96d6ea9
AhlumJahan <ahlumjahan@gmail.com>
2025-06-20 12:24:46 +0530

    lab 7

---
+++ p1.c
@@ -0,0 +1,18 @@
+// swap two numbers using call by refrence
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main()
+{
+
+    int x = 5, y = 10;
+    printf("before swap:x=%d,y=%d\n", x, y);
+    swap(&x, &y); // passing addresses
+    printf("after swap:x=%d,y=%d\n", x, y);
+
+    return 0;
+}
